f2f9b7e20091703126d150a42e6fee2ee2345648
JJ Uzumaki <104718343+xpunk7@users.noreply.github.com>
2022-07-08 14:16:23 +0200

    Create index.js

---
+++ src/countries/index.js
@@ -0,0 +1,256 @@
+const COUNTRIES = [
+  {
+    translationKey: 'the-uk',
+  },
+  {
+    translationKey: 'andorra',
+  },
+  {
+    translationKey: 'argentina',
+  },
+  {
+    translationKey: 'australia',
+  },
+  {
+    translationKey: 'austria',
+  },
+  {
+    translationKey: 'bangladesh',
+  },
+  {
+    translationKey: 'belgium',
+  },
+  {
+    translationKey: 'brazil',
+  },
+  {
+    translationKey: 'british-virgin-islands',
+  },
+  {
+    translationKey: 'bulgaria',
+  },
+  {
+    translationKey: 'canada',
+  },
+  {
+    translationKey: 'chile',
+  },
+  {
+    translationKey: 'china',
+  },
+  {
+    translationKey: 'croatia',
+  },
+  {
+    translationKey: 'cyprus',
+  },
+  {
+    translationKey: 'czech-republic',
+  },
+  {
+    translationKey: 'denmark',
+  },
+  {
+    translationKey: 'dubai',
+  },
+  {
+    translationKey: 'east-timor',
+  },
+  {
+    translationKey: 'ecuador',
+  },
+  {
+    translationKey: 'egypt',
+  },
+  {
+    translationKey: 'el-salvador',
+  },
+  {
+    translationKey: 'estonia',
+  },
+  {
+    translationKey: 'europe',
+  },
+  {
+    translationKey: 'faroe-islands',
+  },
+  {
+    translationKey: 'finland',
+  },
+  {
+    translationKey: 'france',
+  },
+  {
+    translationKey: 'georgia',
+  },
+  {
+    translationKey: 'germany',
+  },
+  {
+    translationKey: 'ghana',
+  },
+  {
+    translationKey: 'gibraltar',
+  },
+  {
+    translationKey: 'greece',
+  },
+  {
+    translationKey: 'hong-kong',
+  },
+  {
+    translationKey: 'hungary',
+  },
+  {
+    translationKey: 'india',
+  },
+  {
+    translationKey: 'indonesia',
+  },
+  {
+    translationKey: 'ireland',
+  },
+  {
+    translationKey: 'israel',
+  },
+  {
+    translationKey: 'italy',
+  },
+  {
+    translationKey: 'japan',
+  },
+  {
+    translationKey: 'kenya',
+  },
+  {
+    translationKey: 'kiribati',
+  },
+  {
+    translationKey: 'latvia',
+  },
+  {
+    translationKey: 'liechtenstein',
+  },
+  {
+    translationKey: 'lithuania',
+  },
+  {
+    translationKey: 'luxembourg',
+  },
+  {
+    translationKey: 'malaysia',
+  },
+  {
+    translationKey: 'malta',
+  },
+  {
+    translationKey: 'marshall-islands',
+  },
+  {
+    translationKey: 'mexico',
+  },
+  {
+    translationKey: 'micronesia',
+  },
+  {
+    translationKey: 'monaco',
+  },
+  {
+    translationKey: 'montenegro',
+  },
+  {
+    translationKey: 'morocco',
+  },
+  {
+    translationKey: 'nepal',
+  },
+  {
+    translationKey: 'the-netherlands',
+  },
+  {
+    translationKey: 'new-zealand',
+  },
+  {
+    translationKey: 'nigeria',
+  },
+  {
+    translationKey: 'norway',
+  },
+  {
+    translationKey: 'pakistan',
+  },
+  {
+    translationKey: 'panama',
+  },
+  {
+    translationKey: 'peru',
+  },
+  {
+    translationKey: 'philippines',
+  },
+  {
+    translationKey: 'poland',
+  },
+  {
+    translationKey: 'portugal',
+  },
+  {
+    translationKey: 'romania',
+  },
+  {
+    translationKey: 'russia',
+  },
+  {
+    translationKey: 'san-marino',
+  },
+  {
+    translationKey: 'singapore',
+  },
+  {
+    translationKey: 'slovakia',
+  },
+  {
+    translationKey: 'slovenia',
+  },
+  {
+    translationKey: 'south-africa',
+  },
+  {
+    translationKey: 'spain',
+  },
+  {
+    translationKey: 'sri-lanka',
+  },
+  {
+    translationKey: 'sweden',
+  },
+  {
+    translationKey: 'switzerland',
+  },
+  {
+    translationKey: 'thailand',
+  },
+  {
+    translationKey: 'turkey',
+  },
+  {
+    translationKey: 'tuvalu',
+  },
+  {
+    translationKey: 'ukraine',
+  },
+  {
+    translationKey: 'united-arab-emirates',
+  },
+  {
+    translationKey: 'the-usa',
+  },
+  {
+    translationKey: 'vatican',
+  },
+  {
+    translationKey: 'vietnam',
+  },
+];
+
+export default COUNTRIES;
